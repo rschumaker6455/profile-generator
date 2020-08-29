d488baea4d919a9187692cab24a6541df90ec39d
Ruth Schumaker <rschumaker6455@gmail.com>
2020-08-29 16:41:41 -0700

    finished templates

--- app.js
+++ app.js
@@ -10,8 +10,174 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const employees = [];
+
+const validateAnswer = async (input) => {
+    if (!input) {
+        return "Please enter a response.";
+    }
+    return true;
+};
+
+const internPrompts = [
+    {
+        type: "input",
+        message: "Intern Name",
+        name: "name",
+        validate: validateAnswer
+    },
+    {
+        type: "input",
+        message: "Intern ID Number",
+        name: "id",
+        validate: validateAnswer
+    },
+    {
+        type: "input",
+        message: "Intern Email Address",
+        name: "email",
+        validate: validateAnswer
+    },
+    {
+        type: "input",
+        message: "Intern School",
+        name: "school",
+        validate: validateAnswer
+    }
+]
+
+const engineerPrompts = [
+    {
+        type: "input",
+        message: "Engineer Name",
+        name: "name",
+        validate: validateAnswer
+    },
+    {
+        type: "input",
+        message: "Engineer ID Number",
+        name: "id",
+        validate: validateAnswer
+    },
+    {
+        type: "input",
+        message: "Engineer Email Address",
+        name: "email",
+        validate: validateAnswer
+    },
+    {
+        type: "input",
+        message: "Engineer Github Profile Name",
+        name: "github",
+        validate: validateAnswer
+    }
+]
+
+const managerPrompts = [
+    {
+        type: "input",
+        message: "Manager Name",
+        name: "name",
+        validate: validateAnswer
+    },
+    {
+        type: "input",
+        message: "Manager ID Number",
+        name: "id",
+        validate: validateAnswer
+    },
+    {
+        type: "input",
+        message: "Manager Email Address",
+        name: "email",
+        validate: validateAnswer
+    },
+    {
+        type: "input",
+        message: "Manager Office Number",
+        name: "officeNumber",
+        validate: validateAnswer
+    }
+]
+
+function newMember() {
+    inquirer.prompt(startQuestions).then(answers => {
+
+        switch (answers.role) {
+            case "Intern":
+                newIntern();
+                break;
+
+            case "Engineer":
+                newEngineer();
+                break;
+
+            case "Manager":
+                newManager();
+                break;
+
+            case "No, I'm done.":
+                console.log("Finished entering team members.");
+                break;
+        
+            default: 
+                newMember();
+        }
+    })
+}
+
+function newIntern() {
+
+    inquirer.prompt(internPrompts).then(answers => {
+        
+        const intern = new Intern (answers.name, answers.id, answers.email, answers.school);
+        employees.push(intern);
+
+        writeToFile();
+        newMember();
+    });
+
+}
+
+function newEngineer() {
+
+    inquirer.prompt(engineerPrompts).then(answers => {
+        
+        const engineer = new Engineer (answers.name, answers.id, answers.email, answers.github);
+        employees.push(engineer);
+
+        writeToFile();
+        newMember();
+    });
+
+}
+
+function newManager() {
+
+    inquirer.prompt(managerPrompts).then(answers => {
+        
+        const manager = new Manager (answers.name, answers.id, answers.email, answers.officeNumber);
+        employees.push(manager);
+
+        writeToFile();
+        newMember();
+    });
+
+}
+
+function writeToFile() {
+    fs.writeFile(outputPath, render(employees), function(err) {
+        if (err) {
+            console.log(err);
+        }
+    });
+}
+
+newMember();
+
 
 // Write code to use inquirer to gather information about the development team members,
+
 // and to create objects for each team member (using the correct classes as blueprints!)
 
 // After the user has input all employees desired, call the `render` function (required
